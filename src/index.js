598d06dd637e42d39a8a58786906626ef477db89
andy <a.chaa@marketinvoice.com>
2016-09-21 17:32:54 +0100

    added redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import createLogger from 'redux-logger';
+import { reducer as formReducer } from 'redux-form';
+
 import App from './App';
 import './index.css';
 
+var rootReducer = combineReducers({
+  form: formReducer
+});
+
+const loggerMiddleware = createLogger();
+
+let store = createStore(rootReducer, {},
+  compose(applyMiddleware(loggerMiddleware), window.devToolsExtension ? window.devToolsExtension() : f => f)
+)
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
